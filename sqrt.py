dc7c1a2a98a29190f2634b2547cd25d0d6655fd7
Milan saju <152895872+milansaju@users.noreply.github.com>
2025-01-20 00:09:13 +0530

    square_root

---
+++ sqrt.py
@@ -0,0 +1,8 @@
+number=int(input("enter a number"))
+if number<0:
+           print("it is a negative number")
+else:
+            sqrtnum=number**0.5
+            print("square root is:",sqrtnum)
+            
+            
