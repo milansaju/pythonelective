bbd7ec958fcc6c6525efe2c07e4328194e35dcae
Milan saju <152895872+milansaju@users.noreply.github.com>
2025-01-20 21:38:45 +0530

    income_tax

--- incometax.py
+++ incometax.py
@@ -1,20 +1,26 @@
-income=float(input("Enter your income:"))
+income=float(input('Enter your Annual Income: '))
 tax=0
-if income<250000:
-    print("NILL")
-elif income<500000:
-    new=income-25000
-    tax=new*0.05
-    print("Your tax:",tax)
-elif income<1000000:
-    new=income-500000
-    tax=new*0.10
-    print("Your tax:",tax)
-elif income<1500000:
-    new=income-1000000
-    tax=new*0.15
-    print("Your tax:",tax)
-elif income>=1000000:
-    new=income-1500000
-    tax=new*0.20
-    print("Your tax:",tax)
+dedu=float(input('Enter deductions (if there\'s Any): '))
+ded_income=income-75000-dedu;
+if(ded_income<300000):
+    print('No Tax to be paid')
+elif(ded_income<700000):
+    extra=float(ded_income-300000)
+    tax=extra*0.05
+    print("Tax to be paid = ", tax )
+elif(ded_income<1000000):
+    extra=float(ded_income-700000)
+    tax=20000+extra*0.10
+    print("Tax to be paid = ", tax )
+elif(ded_income<1200000):
+    extra=float(ded_income-1000000)
+    tax=50000+extra*0.15
+    print("Tax to be paid = ", tax )
+elif(ded_income<1500000):
+    extra=float(ded_income-1500000)
+    tax=80000+extra*0.20
+    print("Tax to be paid = ", tax )
+elif(ded_income>=1500000):
+    extra=float(ded_income-1500000)
+    tax=140000+extra*0.30
+    print("Tax to be paid = ", tax )
